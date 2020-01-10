e2342f92c674cbaec34ccb4baf7538205c56ea0f
PierreBeaujuge <pierre.beaujuge@gmail.com>
2020-01-10 16:48:01 +0000

    fixed sed cmd line to work on other webserver config files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,15 @@
 # Bash script that sets up your web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+# echo "<!DOCTYPE html>\n<html>\n  <head>\n  </head>\n  <body>\n\tHolberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 echo "This is a Test!" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/save_default
-sudo sed -i "/server_name _;/a location /hbnb_static {\nalias /data/web_static/current;\n}\n" /etc/nginx/sites-available/default
+sudo sed -i "/server_name .*;/a location /hbnb_static {\nalias /data/web_static/current;\n}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
